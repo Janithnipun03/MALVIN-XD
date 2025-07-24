47d8ccd4255407291443dec46d6ef10b43f22061
Janithnipun03 <janithnipun200303@gmail.com>
2025-07-25 00:37:54 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~V69D2byL#PglOc3yiaOnHsDlnn8GVTBJcHOMyQLkIB15273VXA0c",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "94768854852", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
